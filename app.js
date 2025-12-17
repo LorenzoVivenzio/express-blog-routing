7519e58c3f8b00c115d31b13df57755a8abd42cf
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-12-17 17:06:53 +0100

    feat: router

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
+import character from "./data.js";
 
 const app = express();
 const port = 3000;
 
+
+
 app.get("/", (req, resp)=>{
     resp.send("ciao")
+})
+
+app.get("/character", (req, resp) =>{
+    const info ={
+        totaleMembri :character.length,
+        character: character,
+    }
+    resp.json(info)
 })
 
 app.listen(port, () => {
---
+++ data.js
@@ -0,0 +1,83 @@
+const character = [
+  {
+    "nome": "Monkey D. Luffy",
+    "ruolo": "Capitano",
+    "frutto": "Gomu Gomu no Mi (Gear 5/Nika)",
+    "descrizione": "Leader carismatico con poteri elastici, sogna di diventare il Re dei Pirati.",
+    "taglia": "3000000000",
+    "attacchi_speciali": ["Gomu Gomu no Bajrang Gun", "Gear 5 Dawn Rocket"]
+  },
+  {
+    "nome": "Roronoa Zoro",
+    "ruolo": "Spadaccino",
+    "frutto": "Nessuno",
+    "descrizione": "Combattente a tre spade, mira a essere il miglior spadaccino del mondo.",
+    "taglia": "1111000000",
+    "attacchi_speciali": ["Santoryu: Oni Giri", "Asura: King of Hell"]
+  },
+  {
+    "nome": "Nami",
+    "ruolo": "Navigatrice",
+    "frutto": "Nessuno",
+    "descrizione": "Esperta di meteorologia, gestisce la mappa e le finanze della ciurma.",
+    "taglia": "366000000",
+    "attacchi_speciali": ["Thunderbolt Tempo", "Black Ball: Zeus"]
+  },
+  {
+    "nome": "Usopp",
+    "ruolo": "Cecchino",
+    "frutto": "Nessuno (Pop Green)",
+    "descrizione": "Inventore e tiratore scelto, noto per le sue bugie epiche.",
+    "taglia": "500000000",
+    "attacchi_speciali": ["Pop Green: Devil Star", "Impact Wolf"]
+  },
+  {
+    "nome": "Vinsmoke Sanji",
+    "ruolo": "Cuoco",
+    "frutto": "Nessuno",
+    "descrizione": "Chef abile nel combattimento con calci, ex-principe Vinsmoke.",
+    "taglia": "1030000000",
+    "attacchi_speciali": ["Diable Jambe: Hell Memories", "Ifrit Jambe"]
+  },
+  {
+    "nome": "Tony Tony Chopper",
+    "ruolo": "Medico",
+    "frutto": "Hito Hito no Mi",
+    "descrizione": "Renna antropomorfa, medico con varie forme.",
+    "taglia": "1000",
+    "attacchi_speciali": ["Monster Point: Kong", "Guard Point"]
+  },
+  {
+    "nome": "Nico Robin",
+    "ruolo": "Archeologa",
+    "frutto": "Hana Hana no Mi",
+    "descrizione": "Legge i Poneglyph, crea arti extra.",
+    "taglia": "930000000",
+    "attacchi_speciali": ["Mil Fleur: Gigante", "Demonio Fleur"]
+  },
+  {
+    "nome": "Franky",
+    "ruolo": "Carpentiere",
+    "frutto": "Nessuno",
+    "descrizione": "Cyborg, pilota della Thousand Sunny.",
+    "taglia": "394000000",
+    "attacchi_speciali": ["Radical Beam", "Franky Iron Boxing"]
+  },
+  {
+    "nome": "Brook",
+    "ruolo": "Musicista",
+    "frutto": "Yomi Yomi no Mi",
+    "descrizione": "Scheletro immortale, violinista.",
+    "taglia": "383000000",
+    "attacchi_speciali": ["Soul Solid", "Hanauta Sancho: Yahazu Giri"]
+  },
+  {
+    "nome": "Jinbe",
+    "ruolo": "Timoniere",
+    "frutto": "Nessuno",
+    "descrizione": "Uomo-pesce, maestro karate gyojin.",
+    "taglia": "438000000",
+    "attacchi_speciali": ["Fish-Man Karate: Shark Tile Punch", "Ocean Current"]
+  }
+]
+export default character;
